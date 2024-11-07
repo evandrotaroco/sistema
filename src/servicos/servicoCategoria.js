02dfe290b87893002d3515bab5c42b77599ebf72
Evandro Taroco <evandrotaroco@hotmail.com>
2024-11-06 23:10:18 -0300

    Update servicoCategoria.js

--- src/servicos/servicoCategoria.js
+++ src/servicos/servicoCategoria.js
@@ -1,4 +1,4 @@
-const urlBase = 'https://bcc-backend-lp-2.vercel.app/categorias';
+const urlBase = 'https://bcc-backend-lp-2-evandro.vercel.app/categorias';
 
 export async function gravarCategoria(categoria) {
     const resposta = await fetch(urlBase, {
